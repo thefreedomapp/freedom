d701bc4e4bfb589f8634e00c3b5bf61db285996c
TheBotlyNoob <thebotlynoob@gmail.com>
2021-07-11 20:38:09 -0400

    fix CI
    Signed-off-by: TheBotlyNoob <thebotlynoob@gmail.com>

--- ci.js
+++ ci.js
@@ -1,13 +1,27 @@
-const builder = require('electron-builder');
-if (process.env.TRAVIS_OS_NAME === 'osx')
-  builder.build({
-    projectDir: 'electron',
-    targets: ['macos']
-  });
-else
-  builder.build({
-    projectDir: 'electron',
-    targets: [process.env.TRAVIS_OS_NAME]
-  });
+const builder = require('electron-builder'),
+  // For local testing
+  os = process.env.TRAVIS_OS_NAME ?? process.platform,
+  Platform = builder.Platform;
 
-console.log(require('glob').sync(`${__dirname}/electron`).join('\n'));
+// If the os is Macos run the builder in Macos mode
+if (os === 'osx')
+  builder.build({
+    projectDir: 'electron',
+    targets: Platform.MAC.createTarget(builder.DIR_TARGET)
+  });
+// If the os is Windows run the builder in Windows mode
+else if (os === 'win32' || os === 'windows')
+  builder.build({
+    projectDir: 'electron',
+    targets: Platform.WINDOWS.createTarget(builder.DIR_TARGET)
+  });
+// If the os is linux run the builder in Linux mode
+else if (os === 'linux')
+  builder.build({
+    projectDir: 'electron',
+    targets: Platform.LINUX.createTarget(builder.DIR_TARGET)
+  });
+// If the os is unsupoported (such as FreeBSD, etc...) say the supported oses
+else throw 'Unsupported Platform! Please Use Linux, Windows, Or Macos.';
+
+console.log(require('glob').sync(`${__dirname}/electron/dist`).join('\n'));
