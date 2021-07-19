7147932cb92e0d5fae441bf9d0e124b28d76f9cd
TheBotlyNoob <thebotlynoob@gmail.com>
2021-07-19 17:38:29 -0400

    update a bit
    Signed-off-by: TheBotlyNoob <thebotlynoob@gmail.com>

--- backend/C/all.cpp
+++ backend/C/all.cpp
@@ -1,3 +1,4 @@
 // This just includes all C/C++ files in this dir, this is useful for building later
 // If you add a new C/C++ file, please include it in this file
 // We are using Nan right now, https://github.com/nodejs/nan please use this instead of the node api
+// If you need to use this file, run: require('backend/C/C')
