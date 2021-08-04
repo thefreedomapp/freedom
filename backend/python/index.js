1bea82b3b9f31fc8074378265a830442b05875f3
TheBotlyNoob <TheBotlyNoob@gmail.com>
2021-08-04 11:59:26 -0400

    promisify

--- backend/python/index.js
+++ backend/python/index.js
@@ -1,16 +1,13 @@
-module.exports = (file, callback, options, ...args) =>
-  // Run init.py with provided arguments
-  require('python-shell').PythonShell.run(
-    'init.py',
-    {
-      ...options,
-      args: [file, ...args],
-      scriptPath: __dirname
-    },
-    (err, result) =>
-      err
-        ? require('../utils/quit')(err)
-        : typeof callback === 'function'
-        ? callback(result)
-        : ''
+module.exports = (file, options, ...args) =>
+  new Promise((resolve, reject) =>
+    // Run init.py with provided arguments
+    require('python-shell').PythonShell.run(
+      'init.py',
+      {
+        ...options,
+        args: [file, ...args],
+        scriptPath: __dirname
+      },
+      (err, result) => (err ? reject(err) : resolve(result))
+    )
   );
