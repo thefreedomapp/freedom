9fa227c53fe7a63448a422d94863c7f10f3cd035
TheBotlyNoob <thebotlynoob@gmail.com>
2021-09-09 15:22:55 -0400

    update

--- frontend/components/layout.jsx
+++ frontend/components/layout.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import io from 'socket.io-client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class Layout extends Component {
   constructor(props) {
@@ -42,7 +43,43 @@ export default class Layout extends Component {
 
   render() {
     return (
-      <div>
+      <>
+        <nav className='nav'>
+          <div className='nav-left'>
+            <div className='logo'>
+              <a href='/'>
+                <span className='freedom'>Freedom</span>
+                <span className='function'>.</span>
+              </a>
+            </div>
+          </div>
+          <div className='nav-middle'>
+            <span data-toggle='modal'>
+              <a href='#projects'>Projects</a>
+            </span>{' '}
+            <span data-toggle='modal'>
+              <a href='https://blog.molai.dev'>Blog</a>
+            </span>
+          </div>
+          <div className='nav-right'>
+            <a
+              href='https://twitter.com/@DevMolai'
+              target='_blank'
+              rel='noreferrer'>
+              <i className='fab fa-twitter' aria-hidden='true'></i>
+            </a>
+            <a
+              href='https://www.youtube.com/channel/UC2VmvzguTHSq232VFAhJGrQ'
+              target='_blank'
+              rel='noreferrer'>
+              <i className='fab fa-youtube' aria-hidden='true'></i>
+            </a>{' '}
+            <a href='mailto:mahir@molai.dev' target='_blank' rel='noreferrer'>
+              <i className='fas fa-envelope' aria-hidden='true'></i>
+            </a>
+          </div>
+        </nav>
+
         {this.state.ready ? (
           this.props.children
         ) : (
@@ -50,7 +87,7 @@ export default class Layout extends Component {
             Loading <b>Freedom</b>, Please Wait...
           </h1>
         )}
-      </div>
+      </>
     );
   }
 }
