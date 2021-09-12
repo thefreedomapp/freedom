b8e64fd96553bcf5a2288fe6798d34ccd27df56d
TheBotlyNoob <thebotlynoob@gmail.com>
2021-09-12 14:15:32 -0400

    fix

--- backend/models/thread.js
+++ backend/models/thread.js
@@ -1,10 +1,11 @@
 const m = require('mongoose');
+const petitions = require('./petition');
 
 module.exports = m.model(
   'thread',
   new m.Schema({
     id: String,
-    messages: Object,
+    messages: petitions,
     petition: Object
   })
 );
