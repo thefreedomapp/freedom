00b9476476035f7beb40ca1b2a66711e154972ee
TheBotlyNoob <thebotlynoob@gmail.com>
2021-09-18 14:38:50 -0400

    fix index page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -2,21 +2,38 @@ import { Component } from 'react';
 import { Layout } from 'components';
 
 export default class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    };
+  }
+
+  mount() {
+    this.setState({ loggedIn });
+  }
+
   render() {
     return (
-      <Layout>
-        {(() => {
-          if(window.loggedIn) {
-            return <h1>Freedom.</h1> ||
-            <h3><a href="/app">Go to the app!</a></h3>
-          } else {
-            return <h1>Freedom.</h1> ||
-            <h3>A man who believes in freedom will do anything under the sun to acquire, or preserve his freedom.
+      <Layout mount={() => this.mount()}>
+        {this.state.loggedIn ? (
+          <>
+            <h1>Freedom.</h1>
+            <h3>
+              <a href='/app'>Go to the app!</a>
+            </h3>
+          </>
+        ) : (
+          <>
+            <h1>Freedom.</h1>
+            <h3>
+              A man who believes in freedom will do anything under the sun to
+              acquire, or preserve his freedom.
               <br />
               <i>- Malcom X</i>
             </h3>
-          }
-        })()}
+          </>
+        )}
       </Layout>
     );
   }
