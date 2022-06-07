1f4455368587866df1906577b78fd79555781cb4
Jay Jackson <jj@jj.is-a.dev>
2022-06-07 17:41:12 -0400

    working websockets

--- server/src/ws.rs
+++ server/src/ws.rs
@@ -1,23 +1,24 @@
-use axum::extract::ws::{WebSocket, WebSocketUpgrade};
+use axum::{
+    extract::ws::{WebSocket, WebSocketUpgrade},
+    response::Response,
+};
 
-pub async fn chat(ws: WebSocketUpgrade) {
-    ws.on_upgrade(inner);
+pub async fn chat(ws: WebSocketUpgrade) -> Response {
     async fn inner(mut socket: WebSocket) {
-        println!("Hi!");
         while let Some(msg) = socket.recv().await {
             let msg = if let Ok(msg) = msg {
                 msg
             } else {
-                println!("err1 {:#?}", msg.unwrap_err());
                 // client disconnected
                 return;
             };
 
             if socket.send(msg).await.is_err() {
-                println!("err2");
                 // client disconnected
                 return;
             }
         }
     }
+
+    ws.on_upgrade(inner)
 }
