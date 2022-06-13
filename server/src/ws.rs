0061aad8b8b7ee10167fda61af9cc8c617e2bb47
Jay Jackson <jj@jj.is-a.dev>
2022-06-13 16:35:48 +0000

    persistant messages

--- server/src/ws.rs
+++ server/src/ws.rs
@@ -1,24 +1,41 @@
 use crate::State;
 use axum::{
-    extract::ws::{Message, WebSocketUpgrade},
+    extract::ws::{Message as WsMessage, WebSocketUpgrade},
     response::Response,
     Extension,
 };
 use futures_util::{stream::StreamExt, SinkExt};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message {
+    pub data: String,
+}
 
 pub async fn chat(
     ws: WebSocketUpgrade,
-    Extension(State { clients, .. }): Extension<State>,
+    Extension(State { clients, db_client }): Extension<State>,
 ) -> Response {
     ws.on_upgrade(|socket| async move {
-        let (sender, mut receiver) = socket.split();
+        let (mut sender, mut receiver) = socket.split();
+
+        let collection = db_client
+            .database("messages")
+            .collection::<Message>("messages");
+
+        if let Ok(mut messages) = collection.find(None, None).await {
+            while let Some(Ok(message)) = messages.next().await {
+                let _ = sender.send(WsMessage::Text(message.data)).await;
+            }
+        }
 
         clients.lock().await.push(sender);
 
         while let Some(Ok(msg)) = receiver.next().await {
             for client in clients.lock().await.iter_mut() {
-                if let Message::Text(_) = msg {
+                if let WsMessage::Text(data) = msg.clone() {
                     let _ = client.send(msg.clone()).await;
+                    let _ = collection.insert_one(Message { data }, None).await;
                 }
             }
         }
