9ead5fba5c846ebb46c58d5ab278f458dc642d4f
Jay Jackson <jj@jj.is-a.dev>
2022-06-19 23:34:56 -0400

    almost working w/backend

---
+++ backend/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server as SocketServer } from "socket.io";
+import next from "next";
+
+const dev =
+    process.env.NODE_ENV !== "production" ||
+    !process.argv.includes("production");
+
+const port = parseInt(process.env.PORT || "3000", 10);
+
+const app = express();
+
+const server = createServer(app);
+
+const io = new SocketServer(server);
+
+// FIXME(@TheBotlyNoob) - remove this once we upgrade to next.js v12.1.7
+process.env.__NEXT_REACT_ROOT = "true";
+const nextApp = next({
+    dev,
+    dir: "../frontend"
+});
+
+nextApp.prepare().then(() => {
+    app.get("*", (req, res) => {
+        nextApp.getRequestHandler()(req, res);
+    });
+});
+
+server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
---
+++ frontend/src/pages/test.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="container">
+            <h1>test</h1>
+        </div>
+    );
+}
