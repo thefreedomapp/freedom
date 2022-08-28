e2959c7610ff3f0089a3bccaa6dacb44efd566a4
Jay Jackson <jj@jj.is-a.dev>
2022-08-28 14:11:46 -0400

    fix a few things

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,10 @@
 
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+	<link
+		href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+		rel="stylesheet"
+	/>
 	<script async src={dev ? "" : "https://arc.io/widget.min.js#iYGzLN9i"}></script>
 
 	<!-- this looks very ugly with dark reader. -->
@@ -25,7 +26,7 @@
 			margin: 0;
 			padding: 0;
 			/*font-family: Raleway, sans-serif;*/
-			font-family: 'Josefin Sans', sans-serif;
+			font-family: "Josefin Sans", sans-serif;
 		}
 	</style>
 </svelte:head>
